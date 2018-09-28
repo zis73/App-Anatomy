75292600cd24c477cdd7e3ce2b4a37c753ee7f1d
zis <desert73eagle@gmail.com>
2018-09-28 14:54:52 +0300

    not ended

--- App Anatomy/ViewController.swift
+++ App Anatomy/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var launchLabel: UILabel!
+    
+    @IBOutlet weak var resignLabel: UILabel!
+    
+    @IBOutlet weak var backgroundLabel: UILabel!
+    
+    @IBOutlet weak var foregroundLabel: UILabel!
+    
+    @IBOutlet weak var activeLabel: UILabel!
+    
+    @IBOutlet weak var terminateLabel: UILabel!
+    
+    var launchCounter = 0
+    var resignCounter = 0
+    var backgroundCounter = 0
+    var foregroundCounter = 0
+    var activeCounter = 0
+    var terminateCounter = 0
+    
+    func updateView(){
+        launchLabel.text = "didFinishLaunch: \(launchCounter)"
+        resignLabel.text = "willResignActive\(resignCounter)"
+        backgroundLabel.text = "didEnterBackground\(backgroundCounter)"
+        foregroundLabel.text = "willEnteredForeground\(foregroundCounter)"
+        activeLabel.text = "didBecomeActive\(activeCounter)"
+        terminateLabel.text = "willTerminate\(terminateCounter)"
     }
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateView()
+    }
 
 }
 
