eea9ad76ac1fd693f80d7bca22105bbc1be27fc3
zis <desert73eagle@gmail.com>
2018-09-28 13:55:28 +0300

    Initial Commit

---
+++ App Anatomy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  App Anatomy
+//
+//  Created by student on 28.09.2018.
+//  Copyright © 2018 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
